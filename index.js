b6c947b59e356a35f69a4dda87394195a239199d
Teniel Smith-Edwards <tsmith1227@outlook.com>
2024-09-29 22:02:47 -0400

    get, get, post setup

--- index.js
+++ index.js
@@ -3,13 +3,32 @@ const app = express();
 const SERVER_PORT = process.env.port || 3000;
 
 // http://localhost:3000/
+app.get('/', (req, res) => {
+    res.send('Welcome to Lab Exercise 4 For COMP3123!');
+})
+
+// http://localhost:3000/hello
 app.get('/hello', (req, res) => {
     res.send('Hello Express JS');
 })
 
-// http://localhost:3000/
-app.post('/', (req, res) => {
-    res.send('POST - Hello World');
+// http://localhost:3000/user?firstname={}&lastname={}
+app.get('/user', (req, res) => {
+    var firstname = req.query.firstname;
+    var lastname = req.query.lastname;
+
+    if(firstname == null && lastname == null){
+        firstname = "Pritesh";
+        lastname = "Patel"
+    }
+    res.send(`{"firstname":"${firstname}", "lastname":"${lastname}"}`);
+})
+
+// http://localhost:3000/user/{fistname}/{lastname}
+app.post('/user/:firstname/:lastname', (req, res) => {
+    const firstname = req.params.firstname;
+    const lastname = req.params.lastname;
+    res.json(`{"firstname":"${firstname}", "lastname":"${lastname}"}`);
 })
 
 
